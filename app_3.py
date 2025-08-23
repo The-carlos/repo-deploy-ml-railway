bc6bf5d941de6911010dc05ee5551cecad49fcde
Carlos Sanchez <carlos-mac@The-Carlos-MacBook-Pro.local>
2025-08-23 08:56:38 -0600

    agregamos la dirección de la base de datos

--- app_3.py
+++ app_3.py
@@ -14,7 +14,8 @@ from datetime import datetime
 import pytz
 
 # Configurar la base de datos
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:yfVsMyfoDbTqzoXXUutIjZBRsuaKRaRW@autorack.proxy.rlwy.net:11691/railway"
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:rMqzPTJcDBQIcUmcghgpmhdzoqHkwkzt@switchyard.proxy.rlwy.net:59725/railway"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 metadata = MetaData()
  
@@ -153,3 +154,4 @@ async def predict_houseprice(file: UploadFile = File(...), db: Session = Depends
     return {
         "predictions": predictions.tolist()
     }
+
